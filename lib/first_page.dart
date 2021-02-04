ee6e25af747d78afe85fbc60970b6227e551ae78
nirmal <nirmal251992@gmail.com>
2021-02-04 15:53:27 +0530

    textfield validation using bloc

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'textBloc.dart';
 
 class FirstPage extends StatefulWidget {
   @override
@@ -8,10 +9,12 @@ class FirstPage extends StatefulWidget {
 
 class _FirstPageState extends State<FirstPage> {
   final counterBloc = CounterBloc();
+  final textBloc = TextBloc();
 
   @override
   void dispose() {
     counterBloc.dispose();
+    textBloc.dispose();
     super.dispose();
   }
 
@@ -23,6 +26,44 @@ class _FirstPageState extends State<FirstPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+                padding: EdgeInsets.all(24.0),
+                child: Center(
+                  child: StreamBuilder(
+                      stream: textBloc.textStream,
+                      builder: (ctxt, AsyncSnapshot<String> textStream) {
+                        return TextField(
+                          onChanged: (String text) => textBloc.updateText(text),
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey[100],
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                              ),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.blueAccent, width: 0.0),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            errorText:
+                                textStream.hasError ? textStream.error : null,
+                            errorBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 0.0),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 0.0),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                          ),
+                        );
+                      }),
+                )),
             Text(
               'Press',
             ),
